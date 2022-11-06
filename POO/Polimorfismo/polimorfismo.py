3b9f22b1b8fa49ae46c67b8257e11d2b950f36be
JheyBi <joaodlrio@outlook.com>
2022-11-06 20:16:24 -0300

    Polimorfismo

---
+++ POO/Polimorfismo/polimorfismo.py
@@ -0,0 +1,19 @@
+class Passaro:
+    def voar(self):
+        print("voando...")
+
+class Pardal(Passaro):
+    def voar(self):
+        super().voar()
+
+
+class Avestruz(Passaro):
+    def voar(self):
+        print("Avestruz não voa!")
+
+
+def plano_voo(obj):
+    obj.voar()
+
+plano_voo(Pardal())
+plano_voo(Avestruz())
